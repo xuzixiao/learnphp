83cb5d7719241b86e450ddee20c4f0a5b516bfae
xuyuanye <git@source.bie-ye.com:comm/bieyewang.git>
2018-08-03 19:53:27 +0800

    all

--- config/config.php
+++ config/config.php
@@ -1,21 +1,9 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
-$username=$_POST["username"];
-$userpwd=$_POST["pwd"];
 define("HOST","localhost");
 define("HOSTNAME","root");
 define("HOSTPWD","root");
 define("DB","study");
 $conn = mysqli_connect(HOST,HOSTNAME,HOSTPWD) or die("Error-数据库连接失败！");
 mysqli_set_charset($conn,"utf8");
-//$sql="insert into user(username,userpwd) values ('".$username."','".$userpwd."')";
-// $sql="CREATE DATABASE PHP";
-// $res=mysqli_query($conn,$sql);
-// if($res){
-//     echo "添加成功".$res;
-// }else{
-//     echo "添加失败!".$res;
-// }
-// mysqli_close($conn);
-
 ?>
--- controller/reg.php
+++ controller/reg.php
@@ -5,7 +5,6 @@ $userpwd=md5($_POST[pwd]);
 mysqli_select_db($conn,"study");
 $sql="insert into user(username,userpwd) values('".$username."','".$userpwd."')";
 $result=mysqli_query($conn,$sql) or die(mysqli_error());
-header("Content-Type: text/html; charset=UTF-8");
 if($result){
     $res=array(
         'success'=>true,
@@ -18,4 +17,5 @@ if($result){
 }else{
     echo "添加失败";
 }
+mysqli_close($conn);
 ?>
